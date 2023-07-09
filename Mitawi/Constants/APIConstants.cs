da8da8c91b7528cb35cb88a78558bc9b599fe4bb
vijaysairaj <114044720+vijaysairaj@users.noreply.github.com>
2023-07-09 11:23:22 +0530

    Update APIConstants.cs

--- Mitawi/Constants/APIConstants.cs
+++ Mitawi/Constants/APIConstants.cs
@@ -3,6 +3,7 @@
     public static class APIConstants
     {
         public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/onecall";
-        public const string OpenWeatherMapAPIKey = "INSERT_API_KEY_HERE";
+        public const string OpenWeatherMapAPIKey = "f84d07b081c4e0f04f79498cb71a0980
+";
     }
 }
